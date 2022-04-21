c7cbd351ba21366775f7eaeace2deefe8ffc1450
Richie Bendall <richiebendall@gmail.com>
2022-04-21 21:27:47 +1200

    Switch to `ava`
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- test.js
+++ test.js
@@ -1,5 +1,4 @@
-import {test} from 'uvu';
-import {equal, is} from 'uvu/assert'; // eslint-disable-line node/file-extension-in-import
+import test from 'ava';
 import pDefer from 'p-defer';
 import {promiseStateAsync as promiseState} from 'p-state';
 import createDeferredAsyncIterator from './index.js';
@@ -12,17 +11,17 @@ function toAsyncIterable(iterator) {
 	};
 }
 
-test('main', async () => {
+test('main', async t => {
 	const {next, complete, onCleanup, iterator} = createDeferredAsyncIterator();
 
 	next(1);
 	next(2);
 
-	equal(await iterator.next(), {
+	t.deepEqual(await iterator.next(), {
 		done: false,
 		value: 1,
 	});
-	equal(await iterator.next(), {
+	t.deepEqual(await iterator.next(), {
 		done: false,
 		value: 2,
 	});
@@ -35,14 +34,14 @@ test('main', async () => {
 		complete();
 	});
 
-	equal(await iterator.next(), {
+	t.deepEqual(await iterator.next(), {
 		done: true,
 	});
 
 	await cleanupPromise;
 });
 
-test('for await...of syntax with .complete()', async () => {
+test('for await...of syntax with .complete()', async t => {
 	const {next, complete, iterator} = createDeferredAsyncIterator();
 
 	setImmediate(() => {
@@ -57,10 +56,10 @@ test('for await...of syntax with .complete()', async () => {
 		values.push(value);
 	}
 
-	equal(values, [1, 2]);
+	t.deepEqual(values, [1, 2]);
 });
 
-test('for await...of syntax with breaking', async () => {
+test('for await...of syntax with breaking', async t => {
 	const {next, iterator} = createDeferredAsyncIterator();
 
 	setImmediate(() => {
@@ -78,20 +77,18 @@ test('for await...of syntax with breaking', async () => {
 		}
 	}
 
-	equal(values, [1, 2]);
+	t.deepEqual(values, [1, 2]);
 });
 
-test('Promise resolves when value is consumed', async () => {
+test('Promise resolves when value is consumed', async t => {
 	const {next, iterator} = createDeferredAsyncIterator();
 	const promise = next(1);
 
-	is(await promiseState(promise), 'pending');
+	t.is(await promiseState(promise), 'pending');
 
 	const {value} = await iterator.next();
 
-	is(value, 1);
+	t.is(value, 1);
 
 	await promise;
 });
-
-test.run();
